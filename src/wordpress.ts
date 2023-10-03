d6a01749e7ca11672603fd37bc118b317bc29a34
Bruce Jones <bruce@orphans.co.uk>
2023-10-03 12:58:29 +0100

    Export icon library and i18n

--- src/wordpress.ts
+++ src/wordpress.ts
@@ -4,4 +4,6 @@ export * as components from "@wordpress/components";
 export * as data from "@wordpress/data";
 export * as element from "@wordpress/element";
 export * as hooks from "@wordpress/hooks";
+export * as i18n from "@wordpress/i18n";
+export * as icons from "@wordpress/icons";
 export { default as serverSideRender } from "@wordpress/server-side-render";
