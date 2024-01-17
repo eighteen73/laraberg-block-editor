27cc2d9cd27ce2719ae6e203ed2b96604e9f3b47
Ed Jeavons <ed@orphans.co.uk>
2024-01-17 12:07:28 +0000

    Fix issue with Notices

--- src/components/Notices.tsx
+++ src/components/Notices.tsx
@@ -1,9 +1,10 @@
 import { createElement } from '@wordpress/element'
 import { useSelect, useDispatch } from '@wordpress/data'
 import { NoticeList } from '@wordpress/components'
+import { store as noticesStore } from '@wordpress/notices';
 
 export default function Notices() {
-    const notices = useSelect((select) => select('core/notices').getNotices())
+    const notices = useSelect((select) => select(noticesStore).getNotices())
     const { removeNotice } = useDispatch('core/notices')
 
     return (
