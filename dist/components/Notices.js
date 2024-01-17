3362b58e585e9538fcfc8f2fa1cf64859aea0a36
Ed Jeavons <ed@orphans.co.uk>
2024-01-17 12:16:39 +0000

    Production assets

--- dist/components/Notices.js
+++ dist/components/Notices.js
@@ -1,8 +1,9 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { useSelect, useDispatch } from '@wordpress/data';
 import { NoticeList } from '@wordpress/components';
+import { store as noticesStore } from '@wordpress/notices';
 export default function Notices() {
-    const notices = useSelect((select) => select('core/notices').getNotices());
+    const notices = useSelect((select) => select(noticesStore).getNotices());
     const { removeNotice } = useDispatch('core/notices');
     return (_jsx(NoticeList, { className: "edit-site-notices", 
         //@ts-ignore
